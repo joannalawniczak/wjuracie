53745705c49d978923ec99b7a7c6505707241a2d
Joanna Ławniczak <lawniczak.joanna@gmail.com>
2016-10-04 17:36:13 +0200

    Added smooth scroll.

--- assets/src/scripts/main.js
+++ assets/src/scripts/main.js
@@ -1,3 +1,4 @@
+import smoothScroll from '../../../node_modules/smooth-scroll/dist/js/smooth-scroll.js';
 import mobileNavigation from './mobile-navigation.js';
 import animatedLabel from './animatedlabel.js';
 import contactForm from './contactform.js';
@@ -6,3 +7,4 @@ import './mapinit.js';
 mobileNavigation( document.querySelector( '.page-header' ) );
 animatedLabel( document.querySelectorAll( 'form .control' ) );
 contactForm( document.querySelector( 'form' ) );
+smoothScroll.init( { speed: 700 } );
